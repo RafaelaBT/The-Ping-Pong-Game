a1641ef8d2f9317fc72b46b0cb75e15c079aabae
Rafaela Barbosa Trindade <rafaelabarbosatrindade@outlook.com>
2024-06-28 23:31:34 -0300

    Adding initial state and ball movement.

--- app/Main.hs
+++ app/Main.hs
@@ -1,52 +1,89 @@
-module Main (main) where
+module Main (main, PingPong, design, initialState) where
 
 -- Import libraries
 import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
 
--- Window shape
-width, height, offset :: Int
+-- Window data
+name :: String
+name = "The Ping-Pong Game"
+
+width, height, offset, fps :: Int
 width = 800
-height = 500
+height = 800
 offset = 10
+fps = 60
 
--- Create window
-window :: Display
-window = InWindow "The Ping-Pong Game" (width, height) (offset, offset)
-
--- Background color
 background :: Color
 background = black
 
--- Create drawing
-drawing :: Picture
-drawing = pictures[
-    -- Objects
-    ball,
-    walls,
-    rightBar,
-    leftBar
-    ]
+-- Data structure
+data PingPong = Game
+    {
+        ballLoc :: (Float, Float),  -- ball location
+        ballVel :: (Float, Float),  -- ball velocity
+        player1 :: Float,           -- left bar height
+        player2 :: Float            -- right bar height
+    }
+    deriving Show -- print values
+
+-- Design
+design :: PingPong -> Picture
+design state = pictures[ball, walls, leftBar $ player1 state, rightBar $ player2 state] -- itens
     where
         -- Ball
-        ball = translate ballRadius ballRadius $ color red $ circleSolid ballRadius
+        ball = uncurry translate (ballLoc state) $ color ballColor $ circleSolid ballRadius
         ballRadius = 10
+        ballColor = red
 
         -- Bottom and top walls
         wall :: Float -> Picture
-        wall offset = translate 0 offset $ color wallColor $ rectangleSolid wallWidth wallHeight
-        wallColor = white
+        wall wallOffset = translate 0 wallOffset $ color wallColor $ rectangleSolid wallWidth wallHeight
+        wallColor = greyN 0.5
         wallWidth = fromIntegral width
         wallHeight = 10
         
         walls = pictures[wall position, wall (-position)]
         position = (fromIntegral height / 2) - (wallHeight / 2)
-
+        
         -- Bars
-        rightBar = translate 380 (0) $ color blue $ rectangleSolid barWidth barHeight
-        leftBar = translate (-380) 0 $ color green $ rectangleSolid barWidth barHeight
+        leftBar, rightBar :: Float -> Picture
+        leftBar x = translate 380 x $ color barColor $ rectangleSolid barWidth barHeight
+        rightBar y = translate (-380) y $ color barColor $ rectangleSolid barWidth barHeight
+        barColor = green
         barWidth = 10
-        barHeight = 300
+        barHeight = 150
+
+-- Initial state
+initialState :: PingPong
+initialState = Game
+    {
+        ballLoc = (0, 0),
+        ballVel = (60, 15),
+        player1 = 0,
+        player2 = 0
+    }
+
+-- Ball animation
+moveBall :: Float -> PingPong -> PingPong
+moveBall seconds state = state {ballLoc = (x', y')}
+    where
+        -- Current location and velocity
+        (x, y) = ballLoc state
+        (vx, vy) = ballVel state
+
+        -- New location and velocity
+        x' = x + vx * seconds
+        y' = y + vy * seconds
+
+-- Define window
+window :: Display
+window = InWindow name (width, height) (offset, offset)
 
 -- Display window
 main :: IO ()
-main = display window background drawing
+main = simulate window background fps initialState design update
+
+-- Update window
+update :: ViewPort -> Float -> PingPong -> PingPong
+update _ = moveBall
